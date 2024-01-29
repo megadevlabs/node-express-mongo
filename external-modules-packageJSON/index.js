a6032ef7aabb9966f09e599d06fc7a8030b8d6fe
Md Salahuddin Khan <cisrony@gmail.com>
2024-01-29 18:45:06 +0600

    Commit for How to use Node.js External Modules from https://npmjs.com

---
+++ external-modules-packageJSON/index.js
@@ -0,0 +1,7 @@
+// Get Random Fruits Name
+const getRandomFruitsName = require('random-fruits-name');
+console.log(getRandomFruitsName('en'));
+
+// Get Random Movies Name
+var moviesNames = require('movies-names');
+console.log(moviesNames.random());
