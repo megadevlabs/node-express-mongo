3cf353509bb42d7a82df514820d97d586977f96c
Md Salahuddin Khan <cisrony@gmail.com>
2024-01-29 17:28:29 +0600

    Commit for Node server create and Run on Browser

---
+++ create-http-server/server.js
@@ -0,0 +1,23 @@
+const http = require('http');
+const port = 3030;
+const hostname = '127.0.0.1';
+
+// Server Create
+
+// Method 1
+// http
+//   .createServer((req, res) => {
+//     res.end("Hello I'm your first server");
+//   })
+//   .listen('3030');
+
+// Method 2
+const myServer = http.createServer((req, res) => {
+  // res.end("Hello I'm your first server");
+  res.end("<h1>Hello I'm your first node server</h1>");
+});
+
+myServer.listen(port, hostname, () => {
+  // console.log('Server is running successfully at localhost:3030');
+  console.log(`Server is running successfully at http://${hostname}:${port}`);
+});
