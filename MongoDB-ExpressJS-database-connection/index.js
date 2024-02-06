dd28ee304224f3fd47cf2fe8200bd2fae871dc17
Md Salahuddin Khan <cisrony@gmail.com>
2024-02-06 18:58:49 +0600

    Commit for save Single Or Multiple data in database from express server

--- MongoDB-ExpressJS-database-connection/index.js
+++ MongoDB-ExpressJS-database-connection/index.js
@@ -3,14 +3,23 @@ const mongoose = require('mongoose');
 const app = express();
 const PORT = 3002;
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 // Mongoose Schema Create
 const productsSchema = new mongoose.Schema({
   title: {
     type: String,
     required: true,
   },
-  price: Number,
-  description: String,
+  price: {
+    type: Number,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
   createdAt: {
     type: Date,
     default: Date.now,
@@ -31,11 +40,56 @@ const connectDB = async () => {
   }
 };
 
+// Listen
 app.listen(PORT, async () => {
   console.log(`Express Server is running at http://localhost:${PORT}`);
   await connectDB();
 });
 
+// Routings
 app.get('/', (req, res) => {
   res.send('<h1>Welcome to MongoDB Home</h1>');
+});
+
+app.post('/products', async (req, res) => {
+  try {
+    // GetData From Request Body Or Form
+
+    // const title = req.body.title;
+    // const price = req.body.price;
+    // const description = req.body.description;
+    const { title, price, description } = req.body;
+
+    // Store Data into Model
+    const newProduct = new Product({
+      title: title,
+      price: price,
+      description: description,
+    });
+    // Save (Single/ One) Data Into Database
+    const productData = await newProduct.save();
+
+    // Save (Multiple/ Many) Data Into Database
+    // const productData = await Product.insertMany([
+    //   {
+    //     title: 'iphone 15',
+    //     price: 1050,
+    //     description: 'This is Nice phone',
+    //   },
+    //   {
+    //     title: 'iphone 15 Pro',
+    //     price: 1250,
+    //     description: 'This is Nice phone',
+    //   },
+    //   {
+    //     title: 'iphone 16 Pro',
+    //     price: 1650,
+    //     description: 'This is Nice phone',
+    //   },
+    // ]);
+
+    res.status(201).send(productData);
+  } catch (error) {
+    res.status(500).send({ message: error.message });
+  }
 });
